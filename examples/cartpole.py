07ac6e32ee3f0725bee380f3485d2f59970ae7c3
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2018-07-11 19:53:01 -0400

    updated cartpole python script

--- examples/cartpole.py
+++ examples/cartpole.py
@@ -1,7 +1,5 @@
 import numpy as np
-import sys
 import torch
-torch.set_num_threads(2)
 
 from functools import partial
 
@@ -9,38 +7,41 @@ from kusanagi.shell import cartpole
 from kusanagi.base import ExperienceDataset, apply_controller
 from kusanagi.ghost.control import RandPolicy
 
-sys.path.append('..')
-from prob_mbrl import algorithms, models, train_regressor, losses, utils
+from prob_mbrl import utils, models, algorithms, losses, train_regressor
+torch.set_num_threads(2)
 
 
-def forward(states, actions, dynamics, measurement_noise=True,
-            z_states=None, z_rewards=None,**kwargs):
-    
-    (deltas, deltas_std), (rewards, rewards_std) = dyn(
-        (states, actions), separate_outputs=True, **kwargs)
+def forward(states, actions, dynamics, **kwargs):
+    deltas, rewards = dynamics(
+        (states, actions), return_samples=True,
+        separate_outputs=True, **kwargs)
     next_states = states + deltas
-    if measurement_noise:
-        z1 = z_states if z_states is not None else torch.randn_like(next_states)
-        next_states += z1*deltas_std
-        #z2 = z_rewards if z_rewards is not None else torch.randn(next_states.shape[0], 1)
-        #rewards += z2*rewards_std
     return next_states, rewards
 
 
-def reward_func(states, target, Q, angle_dims):
+def reward_fn(states, target, Q, angle_dims):
     states = utils.to_complex(states, angle_dims)
     reward = losses.quadratic_saturating_loss(states, target, Q)
-    return reward, torch.zeros_like(reward)
+    return reward
 
 
-# experiment parameters
-n_rnd = 1
-n_opt = 25
+# parameters
 H = 25
-N_particles = 100
-learn_reward=True
+N_particles = 50
+dyn_components = 2
+dyn_hidden = [200]*2
+pol_hidden = [200]*2
+
+# initialize environment
+env = cartpole.Cartpole()
+
+# initialize reward/cost function
+target = torch.tensor([0, 0, 0, np.pi]).float()
+D = target.shape[-1]
+U = 1
+learn_reward = False
+maxU = np.array([10.0])
 angle_dims = torch.tensor([3]).long()
-target = torch.tensor([0,0,0,np.pi]).float()
 target = utils.to_complex(target, angle_dims)
 Da = target.shape[-1]
 Q = torch.zeros(Da, Da).float()
@@ -50,69 +51,81 @@ Q[-2, 0] = env.l
 Q[-2, -2] = env.l**2
 Q[-1, -1] = env.l**2
 Q /= 0.1
-maxU = torch.tensor([10.0])
-D = target.shape[-1]
-U = maxU.shape[-1]
-
-# init environment
-env = cartpole.Cartpole()
-
-# init reward/cost function
 if learn_reward:
-    dynE = 2*(D+1)
     reward_func = None
 else:
-    dynE = 2*D
-    reward_func = partial(reward_func, target=target, Q=Q, angle_dims=angle_dims)
+    reward_func = partial(
+        reward_fn, target=target, Q=Q, angle_dims=angle_dims)
 
-# init dynamics model (heteroscedastic noise)
+
+# initialize dynamics model
+dynE = 2*(D+1) if learn_reward else 2*D
+dyn_model = models.dropout_mlp(
+            Da+U, (dynE+1)*dyn_components, dyn_hidden,
+            dropout_layers=[models.modules.CDropout(0.1, 0.1)]*len(dyn_hidden),
+            nonlin=torch.nn.ReLU,
+            weights_initializer=torch.nn.init.xavier_normal_,
+            biases_initializer=partial(torch.nn.init.uniform_, a=-1.0, b=1.0),
+        )
 dyn = models.DynamicsModel(
-    models.dropout_mlp(
-        D+U, dynE, [200]*2,
-        dropout_layers=[models.modules.CDropout(0.1)]*2,
-        nonlin=torch.nn.ReLU), reward_func=reward_func).float()
-forward_fn = partial(forward, dynamics=dyn)
+    dyn_model, reward_func=reward_func,
+    angle_dims=angle_dims,
+    output_density=models.MixtureDensity(dynE/2, dyn_components)).float()
 
-# init policy
-pol = models.Policy(
-    models.dropout_mlp(
-        D, U, output_nonlin=torch.nn.Tanh,
-        dropout_layers=[models.modules.BDropout(0.1)]*2), maxU).float()
+# initalize policy
+pol_model = models.dropout_mlp(
+        Da, U, pol_hidden,
+        dropout_layers=[models.modules.BDropout(0.1)]*len(pol_hidden),
+        nonlin=torch.nn.ReLU,
+        output_nonlin=torch.nn.Tanh)
+pol = models.Policy(pol_model, maxU, angle_dims=angle_dims).float()
 randpol = RandPolicy(maxU)
 
-# init experience dataset
+# initalize experience dataset
 exp = ExperienceDataset()
 
-# init policy optimizer
+# initialize policy optimizer
 params = filter(lambda p: p.requires_grad, pol.parameters())
-opt = torch.optim.Adam(params, 1e-3, amsgrad=True)
+opt = torch.optim.Adam(params, 1e-4, amsgrad=True)
 
-def cb(*args, **kwargs):
-    env.render()
+# define functions required for rollouts
+forward_fn = partial(forward, dynamics=dyn)
 
-for rand_it in range(n_rnd):
+# collect initial random experience
+for rand_it in range(1):
     ret = apply_controller(
-        env, randpol, H, callback=lambda *args, **kwargs: env.render())
+        env, randpol, H,
+        callback=lambda *args, **kwargs: env.render())
     exp.append_episode(*ret)
 
-for ps_it in range(n_opt):
+
+# policy learning loop
+for ps_it in range(100):
     # apply policy
     ret = apply_controller(
-        env, pol, H, callback=lambda *args, **kwargs: env.render())
+        env, pol, H,
+        callback=lambda *args, **kwargs: env.render())
     exp.append_episode(*ret)
 
     # train dynamics
     X, Y = exp.get_dynmodel_dataset(deltas=True, return_costs=learn_reward)
-    dyn.set_dataset(torch.tensor(X).to(dyn.X.device).float(), torch.tensor(Y).to(dyn.X.device).float())  
-    train_regressor(dyn, 1000, N_particles, True)
-    x0 = torch.tensor(exp.sample_initial_state(N_particles)).to(dyn.X.device).float()
+    dyn.set_dataset(
+        torch.tensor(X).to(dyn.X.device).float(),
+        torch.tensor(Y).to(dyn.X.device).float())
+    train_regressor(
+        dyn, 1000, N_particles, True,
+        log_likelihood=losses.gaussian_mixture_log_likelihood)
+
+    # sample initial states for policy optimization
+    x0 = torch.tensor(
+        exp.sample_states(N_particles, timestep=0)).to(dyn.X.device).float()
     x0 += 1e-2*x0.std(0)*torch.randn_like(x0)
     utils.plot_rollout(x0, forward_fn, pol, H)
-    
+
     # train policy
     print "Policy search iteration %d" % (ps_it+1)
-    algorithms.mc_pilco(x0, forward_fn, dyn, pol, H, opt, exp=exp,
-             maximize=False, pegasus=True, mm_states=True,
-             mm_rewards=True, angle_dims=angle_dims)
+    algorithms.mc_pilco(
+        x0, forward_fn, dyn, pol, H, opt, exp=exp,
+        maximize=False, pegasus=True, mm_states=False,
+        mm_rewards=False, mpc=False, max_steps=25)
     utils.plot_rollout(x0, forward_fn, pol, H)
-    
