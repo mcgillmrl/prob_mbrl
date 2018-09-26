b98d8b65bbfb44eea8e8d640be16deb470b4102d
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2018-09-26 18:09:44 -0400

    refactoring for plotting

--- prob_mbrl/utils.py
+++ prob_mbrl/utils.py
@@ -45,14 +45,48 @@ def to_complex(x, dims=[]):
         return torch.cat([others, angles.sin(), angles.cos()], -1)
 
 
-def plot_rollout(x0, forward, pol, steps):
-    trajs = rollout(
-        x0, forward, pol, steps, resample_model=False,
-        resample_policy=False, resample_particles=False)
-    states, actions, rewards = (
-        torch.stack(x).transpose(0, 1).cpu().detach().numpy()
-        for x in zip(*trajs))
-    names = ['Rolled out States', 'Predicted Actions', 'Predicted Rewards']
+def plot_sample(data, axarr, colors=None, **kwargs):
+    H, D = data.shape
+    plots = []
+    if colors is None:
+        colors = ['steelblue'] * D
+    N = len(colors)
+    for d in range(D):
+        pl, = axarr[d].plot(
+            np.arange(H), data[:, d], color=colors[d % N], **kwargs)
+        plots.append(pl)
+    return plots
+
+
+def plot_mean_var(data, axarr, colors=None, stdevs=2, **kwargs):
+    N, H, D = data.shape
+    plots = []
+    mean = data.mean(0)
+    std = data.std(0)
+    t = np.arange(H)
+    if colors is None:
+        colors = ['steelblue'] * D
+    N = len(colors)
+    for d in range(D):
+        pl, = axarr[d].plot(t, mean[:, d], color=colors[d % N], **kwargs)
+        alpha = kwargs.get('alpha', 0.5)
+        for i in range(1, stdevs + 1):
+            alpha = alpha * 0.8
+            lower_bound = mean[:, d] - i * std[:, d]
+            upper_bound = mean[:, d] + i * std[:, d]
+            axarr[d].fill_between(
+                t, lower_bound, upper_bound, alpha=alpha, color=pl.get_color())
+        plots.append(pl)
+    return plots
+
+
+def plot_trajectories(
+        states,
+        actions,
+        rewards,
+        names=['Rolled out States', 'Predicted Actions', 'Predicted Rewards'],
+        timeout=0.5,
+        plot_samples=True):
     for name in names:
         fig = plt.figure(name)
         fig.clear()
@@ -67,31 +101,41 @@ def plot_rollout(x0, forward, pol, steps):
     axarr1 = [axarr1] if not isinstance(axarr1, Iterable) else axarr1
     axarr2 = [axarr2] if not isinstance(axarr2, Iterable) else axarr2
     axarr3 = [axarr3] if not isinstance(axarr3, Iterable) else axarr3
+    if plot_samples:
+        c1 = c2 = c3 = None
+        for i, (st, ac, rw) in enumerate(zip(states, actions, rewards)):
+            r1 = plot_sample(st, axarr1, c1, alpha=0.3)
+            r2 = plot_sample(ac, axarr2, c2, alpha=0.3)
+            r3 = plot_sample(rw, axarr3, c3, alpha=0.3)
+            c1 = [r.get_color() for r in r1]
+            c2 = [r.get_color() for r in r2]
+            c3 = [r.get_color() for r in r3]
 
-    for i, (st, ac, rw) in enumerate(zip(states, actions, rewards)):
-        H, D = st.shape
-        for d in range(D):
-            axarr1[d].plot(
-                np.arange(H), st[:, d], label='state(%d,%d)' % (d, i),
-                color='steelblue', alpha=0.3)
-
-        H, A = ac.shape
-        for a in range(A):
-            axarr2[a].plot(
-                np.arange(H), ac[:, a], label='action(%d,%d)' % (a, i),
-                color='steelblue', alpha=0.3)
-
-        H, R = rw.shape
-        for r in range(R):
-            axarr3[r].plot(
-                np.arange(H), rw[:, r], label='reward(%d,%d)' % (r, i),
-                color='steelblue', alpha=0.3)
+    else:
+        plot_mean_var(states, axarr1)
+        plot_mean_var(actions, axarr2)
+        plot_mean_var(rewards, axarr3)
 
     for ax in chain(axarr1, axarr2, axarr3):
         ax.figure.canvas.draw()
 
     plt.show(False)
-    plt.waitforbuttonpress(0.5)
+    plt.waitforbuttonpress(timeout)
+
+
+def plot_rollout(x0, forward, pol, steps):
+    trajs = rollout(
+        x0,
+        forward,
+        pol,
+        steps,
+        resample_model=False,
+        resample_policy=False,
+        resample_particles=False)
+    states, actions, rewards = (torch.stack(x).transpose(
+        0, 1).cpu().detach().numpy() for x in zip(*trajs))
+
+    plot_trajectories(states, actions, rewards)
 
 
 def batch_jacobian(f, x, out_dims=None):
@@ -102,6 +146,9 @@ def batch_jacobian(f, x, out_dims=None):
     x_rep = torch.tensor(x_rep, requires_grad=True)
     y_rep = f(x_rep)
     dydx = torch.autograd.grad(
-        y_rep, x_rep, torch.eye(x.shape[-1]),
-        allow_unused=True, retain_graph=True)
+        y_rep,
+        x_rep,
+        torch.eye(x.shape[-1]),
+        allow_unused=True,
+        retain_graph=True)
     return dydx
