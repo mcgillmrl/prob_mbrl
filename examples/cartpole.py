fc3da783afcd3acb79e313afef6fa25f56d06004
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2019-08-09 09:24:11 -0400

    changed default cartpole example to use the dm_control reward

--- examples/cartpole.py
+++ examples/cartpole.py
@@ -14,6 +14,8 @@ if __name__ == '__main__':
     n_rnd = 1
     H = 25
     N_particles = 100
+    N_polopt = 1000
+    N_dynopt = 2000
     dyn_components = 1
     dyn_hidden = [200] * 2
     pol_hidden = [200] * 2
@@ -21,12 +23,11 @@ if __name__ == '__main__':
     learn_reward = False
 
     # initialize environment
-    env = envs.Cartpole()
+    env = envs.mj_cartpole.Cartpole()
     results_filename = os.path.expanduser(
         "~/.prob_mbrl/results_%s_%s.pth.tar" %
         (env.__class__.__name__,
          datetime.datetime.now().strftime("%Y%m%d%H%M%S.%f")))
-    env.dt = env.model.dt
     D = env.observation_space.shape[0]
     U = env.action_space.shape[0]
     maxU = env.action_space.high
@@ -41,36 +42,34 @@ if __name__ == '__main__':
     dynE = 2 * (D + 1) if learn_reward else 2 * D
     if dyn_components > 1:
         output_density = models.MixtureDensity(dynE / 2, dyn_components)
-        dynE = (dynE + 1) * dyn_components
+        dynE = (dynE + 1) * dyn_components + 1
         log_likelihood_loss = losses.gaussian_mixture_log_likelihood
     else:
         output_density = models.DiagGaussianDensity(dynE / 2)
         log_likelihood_loss = losses.gaussian_log_likelihood
 
-    dyn_model = models.mlp(
-        D + U,
-        dynE,
-        dyn_hidden,
-        dropout_layers=[
-            models.modules.CDropout(0.1, 0.1) for i in range(len(dyn_hidden))
-        ],
-        nonlin=torch.nn.Tanh)
-    dyn = models.DynamicsModel(
-        dyn_model, reward_func=reward_func,
-        output_density=output_density).float()
+    dyn_model = models.mlp(D + U,
+                           dynE,
+                           dyn_hidden,
+                           dropout_layers=[
+                               models.modules.CDropout(0.1, 0.1)
+                               for i in range(len(dyn_hidden))
+                           ],
+                           nonlin=torch.nn.ReLU)
+    dyn = models.DynamicsModel(dyn_model,
+                               reward_func=reward_func,
+                               output_density=output_density).float()
 
     # initalize policy
-    pol_model = models.mlp(
-        D,
-        U,
-        pol_hidden,
-        dropout_layers=[
-            models.modules.BDropout(0.1) for i in range(len(pol_hidden))
-        ],
-        nonlin=torch.nn.Tanh,
-        weights_initializer=torch.nn.init.xavier_normal_,
-        biases_initializer=None,
-        output_nonlin=torch.nn.Tanh)
+    pol_model = models.mlp(D,
+                           U,
+                           pol_hidden,
+                           dropout_layers=[
+                               models.modules.BDropout(0.1)
+                               for i in range(len(pol_hidden))
+                           ],
+                           nonlin=torch.nn.ReLU,
+                           output_nonlin=torch.nn.Tanh)
 
     pol = models.Policy(pol_model, maxU).float()
 
@@ -120,47 +119,43 @@ if __name__ == '__main__':
             writer.add_scalar('mc_pilco/episode_%d/training loss' % ps_it,
                               loss, i)
             if i % 100 == 0:
-                states = states.transpose(0, 1).cpu().detach().numpy()
-                actions = actions.transpose(0, 1).cpu().detach().numpy()
-                rewards = rewards.transpose(0, 1).cpu().detach().numpy()
-                utils.plot_trajectories(
-                    states, actions, rewards, plot_samples=False)
+                writer.flush()
 
         # train dynamics
         X, Y = exp.get_dynmodel_dataset(deltas=True, return_costs=learn_reward)
-        dyn.set_dataset(
-            torch.tensor(X).to(dyn.X.device).float(),
-            torch.tensor(Y).to(dyn.X.device).float())
-        utils.train_regressor(
-            dyn,
-            2000,
-            N_particles,
-            True,
-            opt1,
-            log_likelihood=log_likelihood_loss)
+        dyn.set_dataset(X.to(dyn.X.device).float(), Y.to(dyn.X.device).float())
+        utils.train_regressor(dyn,
+                              N_dynopt,
+                              N_particles,
+                              True,
+                              opt1,
+                              log_likelihood=log_likelihood_loss,
+                              summary_writer=writer,
+                              summary_scope='model_learning/episode_%d' %
+                              ps_it)
 
         # sample initial states for policy optimization
-        x0 = torch.tensor(exp.sample_states(N_particles, timestep=0)).to(
-            dyn.X.device).float()
+        x0 = exp.sample_states(N_particles,
+                               timestep=0).to(dyn.X.device).float()
         x0 = x0 + 1e-1 * x0.std(0) * torch.randn_like(x0)
         x0 = x0.detach()
         utils.plot_rollout(x0, dyn, pol, H)
 
         # train policy
         print("Policy search iteration %d" % (ps_it + 1))
-        algorithms.mc_pilco(
-            x0,
-            dyn,
-            pol,
-            H,
-            opt2,
-            exp,
-            1000,
-            pegasus=True,
-            mm_states=True,
-            mm_rewards=True,
-            maximize=True,
-            clip_grad=1.0)
+        algorithms.mc_pilco(x0,
+                            dyn,
+                            pol,
+                            H,
+                            opt2,
+                            exp,
+                            N_polopt,
+                            pegasus=True,
+                            mm_states=False,
+                            mm_rewards=False,
+                            maximize=True,
+                            clip_grad=1.0,
+                            on_iteration=on_iteration)
         utils.plot_rollout(x0, dyn, pol, H)
         writer.add_scalar('robot/evaluation_loss',
                           torch.tensor(ret[2]).sum(), ps_it + 1)
