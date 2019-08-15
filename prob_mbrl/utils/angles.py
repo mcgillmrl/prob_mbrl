e75b217d3df28c15de2706cb85a186ef14360383
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2019-08-14 21:55:52 -0400

    update for pytorhc 1.2

--- prob_mbrl/utils/angles.py
+++ prob_mbrl/utils/angles.py
@@ -4,7 +4,7 @@ import torch
 ODIMS = {}
 
 
-def to_complex(x, dims=[]):
+def to_complex(x, dims):
     global ODIMS
     if len(dims) == 0:
         return x
@@ -14,18 +14,28 @@ def to_complex(x, dims=[]):
         if odims_id in ODIMS:
             odims, dims = ODIMS[odims_id]
         else:
-            # build indices for other dimensions
-            dims = torch.tensor(dims).to(x.device)
-            odims = torch.arange(0, x.shape[-1]).long().to(dims.device)
-            odims = (
-                1 - torch.eq(dims, odims[:, None])).prod(1).nonzero()[:, 0]
-            ODIMS[odims_id] = (odims.detach(), dims)
+            odims, dims = build_odims_(x, dims)
+            ODIMS[odims_id] = (odims, dims)
 
         if isinstance(x, torch.Tensor):
-            angles = x.index_select(-1, dims)
-            others = x.index_select(-1, odims)
-            return torch.cat([others, angles.sin(), angles.cos()], -1)
+            return to_complex_(x, dims, odims)
         else:
             angles = np.atleast_1d(x[..., dims])
             others = np.atleast_1d(x[..., odims])
             return np.concatenate([others, np.sin(angles), np.cos(angles)], -1)
+
+
+def build_odims_(x, dims):
+    # build indices for other dimensions
+    if not isinstance(dims, torch.Tensor):
+        dims = torch.tensor(dims).to(x.device)
+    odims = torch.arange(0, x.shape[-1]).long().to(dims.device)
+    odims = torch.bitwise_not(torch.eq(dims,
+                                       odims[:, None])).prod(1).nonzero()[:, 0]
+    return odims.detach(), dims
+
+
+def to_complex_(x, dims, odims):
+    angles = x.index_select(-1, dims)
+    others = x.index_select(-1, odims)
+    return torch.cat([others, angles.sin(), angles.cos()], -1)
