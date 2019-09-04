cb5425792d7df8bf47c7bb2d7afd55e3e41ecdb2
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2019-09-04 15:51:26 -0400

    added mm groups added tile op

--- prob_mbrl/utils/__init__.py
+++ prob_mbrl/utils/__init__.py
@@ -1,5 +1,5 @@
 from .core import (plot_sample, plot_mean_var, plot_trajectories, plot_rollout,
-                   batch_jacobian, polyak_averaging, sin_squashing_fn,
+                   batch_jacobian, polyak_averaging, sin_squashing_fn, tile,
                    load_checkpoint)
 from .train_regressor import train_regressor, iterate_minibatches
 from .rollout import rollout, rollout_with_Qvalues
@@ -10,7 +10,7 @@ from . import (classproperty, angles)
 __all__ = [
     "iterate_minibatches", "plot_sample", "plot_mean_var", "plot_trajectories",
     "plot_rollout", "batch_jacobian", "polyak_averaging", "sin_squashing_fn",
-    "load_checkpoint", "classproperty", "angles", "ExperienceDataset",
+    "tile", "load_checkpoint", "classproperty", "angles", "ExperienceDataset",
     "SumTree", "apply_controller", "custom_pbar", "train_regressor", "rollout",
     "rollout_with_Qvalues"
 ]
