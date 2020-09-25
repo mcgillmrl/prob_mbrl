6d816e99cd2c34f5be54985eea87ce7b05c77ac8
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2020-09-25 15:49:59 -0400

    updated example

--- examples/bnn_regression.py
+++ examples/bnn_regression.py
@@ -8,36 +8,6 @@ from scipy.special import logsumexp
 from prob_mbrl import models, utils
 
 
-def gaussian_sample(mu, log_sigma):
-    z2 = np.random.randn(*mu.shape)
-    return mu + z2 * np.exp(log_sigma)
-
-
-def mixture_sample(mu, log_sigma, logit_pi, colors=None, noise=True):
-    z1 = np.random.rand(*logit_pi.shape)
-    g1 = -np.log(-np.log(z1))
-    k = (logit_pi - logsumexp(logit_pi, -1)[:, None] + g1).argmax(-1)
-    idx = np.arange(len(mu))
-    samples = mu[idx, k]
-    if noise:
-        z2 = np.random.randn(*mu.shape[:-1])
-        samples += z2 * np.exp(log_sigma[idx, k])
-
-    if colors is not None:
-        return samples, colors[k]
-    return samples
-
-
-def f(x, multimodal=False):
-    c = 100
-    if multimodal:
-        c *= np.random.choice([-1, 1], x.shape[0])
-    return c * sum([
-        np.sin(-2 * np.pi * (2 * k - 1) * x) / (2 * k - 1)
-        for k in range(1, 3)
-    ])
-
-
 def main():
     # model parameters
     parser = argparse.ArgumentParser("BNN regression example")
@@ -51,7 +21,7 @@ def main():
     parser.add_argument('--n_components', type=int, default=5)
     parser.add_argument('--N_batch', type=int, default=100)
     parser.add_argument('--train_iters', type=int, default=10000)
-    parser.add_argument('--noise_level', type=float, default=1e-1)
+    parser.add_argument('--noise_level', type=float, default=0)
     parser.add_argument('--resample', action='store_true')
     parser.add_argument('--use_cuda', action='store_true')
     args = parser.parse_args()
@@ -61,151 +31,146 @@ def main():
     torch.set_num_threads(args.num_threads)
     torch.set_flush_denormal(True)
 
-    idims, odims = 1, 1
-    # single gaussian output model
-    mlp = models.mlp(idims,
-                     2 * odims,
-                     args.net_shape,
-                     dropout_layers=[
-                         models.CDropout(args.drop_rate * np.ones(hid))
-                         for hid in args.net_shape
-                     ])
-    model = models.Regressor(mlp,
-                             output_density=models.DiagGaussianDensity(odims))
+    if args.use_cuda:
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
-    # mixture density network
-    mlp2 = models.mlp(idims,
-                      2 * args.n_components * odims + args.n_components + 1,
-                      args.net_shape,
-                      dropout_layers=[
-                          models.CDropout(args.drop_rate * np.ones(hid))
-                          for hid in args.net_shape
-                      ])
-    mmodel = models.Regressor(mlp2,
-                              output_density=models.GaussianMixtureDensity(
-                                  odims, args.n_components))
-
-    # optimizer for single gaussian model
-    opt1 = torch.optim.Adam(model.parameters(), args.lr)
-
-    # optimizer for mixture density network
-    opt2 = torch.optim.Adam(mmodel.parameters(), args.lr)
+    # create toy dataset
+    def f(x, multimodal=False):
+        c = 100
+        if multimodal:
+            c *= 2 * torch.randint_like(x, 2) - 1
+        return c * sum([
+            torch.sin(-2 * np.pi * (2 * k - 1) * x) / (2 * k - 1)
+            for k in range(1, 3)
+        ])
 
     # create training dataset
-    train_x = np.concatenate([
-        np.linspace(-1.6, -0.25, 100),
-        np.linspace(0.1, 0.25, 100),
-        np.linspace(0.65, 1.0, 100)
+    train_x = torch.cat([
+        torch.arange(-0.6, -0.25, 0.01),
+        torch.arange(0.1, 0.45, 0.005),
+        torch.arange(0.7, 1.25, 0.01)
     ])
-    train_y = f(train_x)
-    train_y += args.noise_level * np.random.randn(*train_y.shape)
-    X = torch.from_numpy(train_x[:, None]).float()
-    Y = torch.from_numpy(train_y[:, None]).float()
-
-    model.set_dataset(X, Y)
-    mmodel.set_dataset(X, Y)
-
-    model = model.float()
-    mmodel = mmodel.float()
-
-    if args.use_cuda and torch.cuda.is_available():
-        X = X.cuda()
-        Y = Y.cuda()
-        model = model.cuda()
-        mmodel = mmodel.cuda()
-
+    train_y = f(train_x, False)
+    train_y += 0.01 * torch.randn(*train_y.shape)
+    X = train_x[:, None]
+    Y = train_y[:, None]
+    Y = Y + torch.randn_like(Y) * args.noise_level
+    plt.scatter(X.cpu(), Y.cpu())
+    xx = torch.linspace(-.1 + X.min(), .1 + X.max())
+    yy = f(xx)
+    plt.plot(xx.cpu(), yy.cpu(), linestyle='--')
+    # plt.plot(xx.cpu(), -yy.cpu(), linestyle='--')
     print(('Dataset size:', train_x.shape[0], 'samples'))
-    # train unimodal regressor
-    utils.train_regressor(model,
-                          iters=args.train_iters,
-                          batchsize=args.N_batch,
-                          resample=args.resample,
-                          optimizer=opt1,
-                          log_likelihood=model.output_density.log_prob)
 
-    # evaluate single gaussian model
-    test_x = np.arange(-2.0, 1.5, 0.005)
-    ret = []
-    if args.resample:
-        model.resample()
-    for i, x in enumerate(test_x):
-        x = torch.tensor(x[None]).float().to(model.X.device)
-        outs = model(x.expand((2 * args.N_batch, 1)), resample=False)
-        y = torch.cat(outs[:2], -1)
-        ret.append(y.cpu().detach().numpy())
-        torch.cuda.empty_cache()
-    ret = np.stack(ret)
-    ret = ret.transpose(1, 0, 2)
-    torch.cuda.empty_cache()
-    for i in range(3):
-        gc.collect()
-
-    plt.figure(figsize=(16, 9))
-    nc = ret.shape[-2]
-    colors = np.array(list(plt.cm.rainbow_r(np.linspace(0, 1, nc))))
-    for i in range(len(ret)):
-        m, logS = ret[i, :, 0], ret[i, :, 1]
-        samples = gaussian_sample(m, logS)
-        plt.scatter(test_x, m, c=colors[0:1], s=1)
-        plt.scatter(test_x, samples, c=colors[0:1] * 0.5, s=1)
-    plt.plot(test_x, f(test_x), linestyle='--', label='true function')
-    plt.scatter(X.cpu().numpy().flatten(), Y.cpu().numpy().flatten())
-    plt.xlabel('$x$', fontsize=18)
-    plt.ylabel('$y$', fontsize=18)
-
+    # single gaussian model
+    input_dims = 1
+    output_dims = 1
+    hids = args.net_shape
+    net = models.mlp(input_dims,
+                     models.GaussianDN.n_params(output_dims),
+                     hids,
+                     dropout_layers=[
+                         models.CDropout(args.drop_rate * torch.ones(hid))
+                         for hid in hids
+                     ],
+                     nonlin=models.activations.hhSinLU)
+    model = models.GaussianDN(net)
+    model.set_scaling(X, Y)
     print(model)
 
-    # train mixture regressor
-    utils.train_regressor(mmodel,
-                          iters=args.train_iters,
-                          batchsize=args.N_batch,
-                          resample=args.resample,
-                          optimizer=opt2,
-                          log_likelihood=mmodel.output_density.log_prob)
+    opt = torch.optim.Adam(model.parameters(), lr=args.lr)
+    utils.train_model(model,
+                      X,
+                      Y,
+                      n_iters=args.train_iters,
+                      opt=opt,
+                      resample=args.resample,
+                      batch_size=args.N_batch)
+    print(model)
 
-    # evaluate mixture density network
-    test_x = np.arange(-2.0, 1.5, 0.005)
-    ret = []
-    logit_weights = []
-    if args.resample:
-        mmodel.resample()
-    for i, x in enumerate(test_x):
-        x = torch.tensor(x[None]).float().to(mmodel.X.device)
-        outs = mmodel(x.expand((2 * args.N_batch, 1)), resample=False)
-        y = torch.cat(outs[:2], -2)
-        ret.append(y.cpu().detach().numpy())
-        logit_weights.append(outs[2].cpu().detach().numpy())
-        torch.cuda.empty_cache()
-    ret = np.stack(ret)
-    ret = ret.transpose(1, 0, 2, 3)
-    logit_weights = np.stack(logit_weights)
-    logit_weights = logit_weights.transpose(1, 0, 2)
-    torch.cuda.empty_cache()
-    for i in range(3):
-        gc.collect()
-
-    plt.figure(figsize=(16, 9))
-    nc = ret.shape[-1]
-    colors = np.array(list(plt.cm.rainbow_r(np.linspace(0, 1, nc))))
-    total_samples = []
-    for i in range(len(ret)):
-        m, logS = ret[i, :, 0, :], ret[i, :, 1, :]
-        samples, c = mixture_sample(m, logS, logit_weights[i], colors)
-        plt.scatter(test_x, samples, c=c * 0.5, s=1)
-        samples, c = mixture_sample(m,
-                                    logS,
-                                    logit_weights[i],
-                                    colors,
-                                    noise=False)
-        plt.scatter(test_x, samples, c=c, s=1)
-        total_samples.append(samples)
-    total_samples = np.array(total_samples)
-    plt.plot(test_x, f(test_x), linestyle='--', label='true function')
-    plt.scatter(X.cpu().numpy().flatten(), Y.cpu().numpy().flatten())
-    plt.xlabel('$x$', fontsize=18)
-    plt.ylabel('$y$', fontsize=18)
-
+    # mixture of gaussians model
+    nc = args.n_components
+    net = models.mlp(input_dims,
+                     models.GaussianMDN.n_params(output_dims, nc),
+                     hids,
+                     dropout_layers=[
+                         models.CDropout(args.drop_rate * torch.ones(hid))
+                         for hid in hids
+                     ],
+                     nonlin=models.activations.hhSinLU)
+    mmodel = models.GaussianMDN(net, nc)
+    mmodel.set_scaling(X, Y)
     print(mmodel)
+
+    opt = torch.optim.Adam(mmodel.parameters(), lr=args.lr)
+    utils.train_model(mmodel,
+                      X,
+                      Y,
+                      n_iters=args.train_iters,
+                      opt=opt,
+                      resample=args.resample,
+                      batch_size=args.N_batch)
+    print(mmodel)
+
+    # plot results for single gaussian model
+    xx = torch.linspace(-2.5 + X.min(), 2.5 + X.max(), 500)
+    xx = xx[:, None, None].repeat(1, 100, 1)
+    with torch.no_grad():
+        model.resample()
+        yy_pred = model(xx, temperature=1.0, resample=False)
+        noiseless_yy_pred = model(xx, temperature=1.0e-9, resample=False)
+    xx = xx.cpu()
+    colors = np.array(list(plt.cm.rainbow_r(np.linspace(0, 1, 255))))
+
+    fig = plt.figure(figsize=(16, 9))
+    means = yy_pred[2]['mu'].squeeze(-1).cpu()
+    stds = yy_pred[2]['sqrtSigma'].diagonal(0, -1, -2).squeeze(-1).cpu()
+    plt.plot(xx.squeeze(-1), means, c=colors[0], alpha=0.1)
+    for i in range(means.shape[1]):
+        plt.fill_between(xx[:, i].squeeze(-1),
+                         means[:, i] - stds[:, i],
+                         means[:, i] + stds[:, i],
+                         color=0.5 * colors[0],
+                         alpha=0.1)
+    plt.scatter(X.cpu(), Y.cpu())
+    yy = f(xx[:, 0]).cpu()
+    plt.plot(xx[:, 0], yy, linestyle='--')
+    plt.ylim(1.5 * yy.min(), 1.5 * yy.max())
+
+    # plot results for gaussian mixture model
+    xx = torch.linspace(-2.5 + X.min(), 2.5 + X.max(), 500)
+    xx = xx[:, None, None].repeat(1, 100, 1)
+    with torch.no_grad():
+        mmodel.resample()
+        yy_pred = mmodel(xx, temperature=1.0, resample=False)
+        noiseless_yy_pred = mmodel(xx, temperature=1.0e-9, resample=False)
+    xx = xx.cpu()
+
+    fig = plt.figure(figsize=(16, 9))
+    ax = fig.gca()
+    colors = np.array(list(plt.cm.rainbow_r(np.linspace(0, 1, 255))))
+    means = yy_pred[2]['mu'].squeeze(-1).cpu()
+    stds = yy_pred[2]['sqrtSigma'].diagonal(0, -1, -2).squeeze(-1).cpu()
+    logit_pi = yy_pred[2]['logit_pi'].squeeze(-1).cpu()
+
+    # plot samples from the mixture
+    pi = torch.log_softmax(logit_pi, -1).exp()
+    comp = pi.max(-1, keepdim=True)[1]
+    ret = plt.scatter(xx.squeeze(-1),
+                      yy_pred[0].squeeze(-1).cpu(),
+                      c=0.5 * colors[0:1],
+                      alpha=0.1,
+                      s=1)
+    ret = plt.scatter(xx.squeeze(-1),
+                      noiseless_yy_pred[0].squeeze(-1).cpu(),
+                      c=colors[0:1],
+                      alpha=0.1,
+                      s=1)
+
+    plt.scatter(X.cpu(), Y.cpu())
+    yy = f(xx[:, 0]).cpu()
+    plt.plot(xx[:, 0], yy, linestyle='--')
+    ret = plt.ylim(1.5 * yy.min(), 1.5 * yy.max())
 
     plt.show()
 
